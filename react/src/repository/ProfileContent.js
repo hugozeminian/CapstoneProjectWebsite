db052bd1e9df09dbceb93638c613bf3276559f5c
Uyara Montovaneli <uyaramontovaneli@gmail.com>
2024-03-30 17:21:40 -0600

    imgs

--- react/src/repository/ProfileContent.js
+++ react/src/repository/ProfileContent.js
@@ -3,18 +3,18 @@ import profileImg from "../assets/img/profile.png"
 const ProfileContent = {
     section1_profile: [
         {
-            img: profileImg,
+            image_path: profileImg,
             title: "Michelle Meghie Dawn",
-            desc: "I find joy in connecting with people from diverse backgrounds, embracing their stories, cultures, and beliefs with open arms. My warm smile fosters a welcoming atmosphere where positive connections flourish. With a Bachelor of Arts in Sociology and years of experience in social services, I've honed my empathy and understanding of those facing challenges. Each interaction is met with genuine patience and a deep respect for your unique journey. In my role as an event planner, I bring enthusiasm and creativity to craft unforgettable experiences. Your special day is my canvas, and I'm dedicated to making it a celebration that reflects your individuality and leaves a lasting impression on you and your loved ones.",
+            description: "I find joy in connecting with people from diverse backgrounds, embracing their stories, cultures, and beliefs with open arms. My warm smile fosters a welcoming atmosphere where positive connections flourish. With a Bachelor of Arts in Sociology and years of experience in social services, I've honed my empathy and understanding of those facing challenges. Each interaction is met with genuine patience and a deep respect for your unique journey. In my role as an event planner, I bring enthusiasm and creativity to craft unforgettable experiences. Your special day is my canvas, and I'm dedicated to making it a celebration that reflects your individuality and leaves a lasting impression on you and your loved ones.",
             ref: "section1_MicheleProfile",
         },
     ],
 
     section2_youtube: [
         {
-            img: "",
+            image_path: "",
             title: "",
-            desc: "",
+            description: "",
             video: "p7EY5giCIlU",
             ref: "",
         },
@@ -22,42 +22,42 @@ const ProfileContent = {
 
     section3_partners_title: [
         {
-            img: "",
+            image_path: "",
             title: "Our Partners",
-            desc: "",
+            description: "",
             ref: "",
         },
     ],
 
     section3_partners: [
         {
-            img: "https://via.placeholder.com/600x400?text=Image",
+            image_path: "https://via.placeholder.com/600x400?text=Image",
             title: "",
-            desc: "",
+            description: "",
             ref: "",
         },
         {
-            img: "https://via.placeholder.com/600x400?text=Image",
+            image_path: "https://via.placeholder.com/600x400?text=Image",
             title: "",
-            desc: "",
+            description: "",
             ref: "",
         },
         {
-            img: "https://via.placeholder.com/600x400?text=Image",
+            image_path: "https://via.placeholder.com/600x400?text=Image",
             title: "",
-            desc: "",
+            description: "",
             ref: "",
         },
         {
-            img: "https://via.placeholder.com/600x400?text=Image",
+            image_path: "https://via.placeholder.com/600x400?text=Image",
             title: "",
-            desc: "",
+            description: "",
             ref: "",
         },
         {
-            img: "https://via.placeholder.com/600x400?text=Image",
+            image_path: "https://via.placeholder.com/600x400?text=Image",
             title: "",
-            desc: "",
+            description: "",
             ref: "",
         },
     ]
