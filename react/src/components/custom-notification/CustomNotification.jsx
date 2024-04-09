f7469da3eb5ae5a36a24302da335c8875045ef7e
hugozaminian <hugozamian@hotmail.com>
2024-04-09 17:29:35 -0600

    form get quote show message after submit

---
+++ react/src/components/custom-notification/CustomNotification.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const CustomNotification = ({ message, severity, show = false }) => {
+  const [open, setOpen] = useState(show);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    setOpen(show);
+  }, [show]);
+
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+    >
+      <MuiAlert
+        elevation={6}
+        variant="filled"
+        onClose={handleClose}
+        severity={severity}
+        style={
+          severity === "success"
+            ? { backgroundColor: "#C29B24", color: "#fff" }
+            : null
+        }
+      >
+        {message}
+      </MuiAlert>
+    </Snackbar>
+  );
+};
+
+export default CustomNotification;
