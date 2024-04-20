efa14e70ced3ecaf28e4bdaa962c352f847d2ee7
leandroevaristo <evaristocordas@gmail.com>
2024-04-19 22:19:18 -0600

    app database import file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,20 +5,24 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
-    /**
+ /**
      * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-         \App\Models\User::factory(30)->create();
+        \App\Models\User::factory(30)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
-        // ]);
+        // ]);}
+
     }
 }
+   
+
